6c7d44e5e2cea7e67c7a83d6791ec28252e8cbd2
unknown <drhansen000@hotmail.com>
2018-02-19 20:05:10 -0700

    got rid of commented out code

--- displayNumbers/app/src/main/java/com/example/drhan/displaynumbers/MainActivity.java
+++ displayNumbers/app/src/main/java/com/example/drhan/displaynumbers/MainActivity.java
@@ -57,84 +57,8 @@ public class MainActivity extends AppCompatActivity
             }
         }
         );
-
-
-//        // First create the List to fulfill the parameter requirements of the ArrayAdapter
-//        numberList = new ArrayList<>();
-//        //pass in this context, the layout for each textView, and the numberList that was just
-//        //instantiated
-//        numberListAdapter = new ArrayAdapter<Integer>(this, R.layout.item_layout, numberList);
-//
-//        // Now connect the ArrayAdapter to the ListView
-//        ListView listView = (ListView) findViewById(R.id.listView);
-//        listView.setAdapter(numberListAdapter);
     }
 
-//    /**
-//     *
-//     * @param view
-//     */
-//    public void createFile(View view) {
-//        progressBar.setProgress(0);
-//
-//        try {
-//            FileOutputStream outputStream;
-//            String insertLine = new String();
-//
-//            //use the output stream so that we can open and write to a specified file
-//            outputStream = openFileOutput(FILE, Context.MODE_PRIVATE);
-//
-//            for(int i = 1; i <= 10; i++) {
-//                //format is a number, then a newline
-//                insertLine = insertLine.format("%d%n", i);
-//                //write to the file, converting the string into bytes
-//                outputStream.write(insertLine.getBytes());
-//                //update the progressBar
-//                progressBar.incrementProgressBy(10);
-//                //simulate heavy program by adding a time delay
-//                Thread.sleep(250);
-//            }
-//            //close the file
-//            outputStream.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        if(progressBar.getProgress() == 100) {
-//
-//        }
-//    }
-
-//    public void loadFile(View view) {
-//        //create a string that will take in the file's values one line at a time
-//        String fileLine;
-//        try {
-//            //Open the file and put it into an inputStream
-//            FileInputStream inputStream = this.openFileInput(FILE);
-//            //create BufferedReader to read specified file
-//            BufferedReader readFileVar = new BufferedReader(new InputStreamReader(inputStream));
-//            //initiate the progress bar at 0
-//            progressBar.setProgress(0);
-//            while ((fileLine = readFileVar.readLine()) != null) {
-//                //convert the file line into an Integer
-//                Integer i = Integer.parseInt(fileLine);
-//                //add the Integer to the adapter
-//                numberListAdapter.add(i);
-//                //increment the progress bar
-//                progressBar.incrementProgressBy(i * 10);
-//                //simulate a heavier load
-//                Thread.sleep(250);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        //let the user know that we're done loading
-//        if (progressBar.getProgress() == 100) {
-//            Toast.makeText(getApplicationContext(), "Finished Loading", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
     public void clearList(View view) {
         // Reset the progress bar
         progressBar.setProgress(0);
